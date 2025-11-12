e75f97fbcf0887fcdbb6b861a1fb8e061233a758
Slava <viavhaslaukandratsenka@gmail.com>
2025-11-12 20:18:38 +0300

    Update calculator.cpp

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -1,2 +1,105 @@
 // В этом файле определения функций.
 // За основу возьмите решение предыдущей задачи.
+#include <iostream>
+#include "calculator.h"
+#include <string>
+#include <cmath>
+#include <limits>
+using namespace std;
+
+bool ReadNumber(Number& result){
+    if(!(cin >> result)){
+        cerr<<"Error: Numeric operand expected"<<endl;
+        return false; 
+    }
+    return true; 
+}
+
+bool RunCalculatorCycle(){
+    string command;
+    Number current_number; 
+    Number saved_number = 0; 
+    bool number_is_saved = false;
+
+    if(!(ReadNumber(current_number))){
+        return false;
+    }
+    while (cin>>command){
+        if(command == "s"){
+            saved_number = current_number;
+            number_is_saved = true;
+        }
+        else if(command == "l"){
+            if(number_is_saved){
+                current_number = saved_number;
+            }
+            else{
+                cerr<<"Error: Memory is empty"<<endl;
+                return false;
+            }
+        }
+        else if(command == "+"){
+            Number user_number; // число введённое пользователем
+            if(!(ReadNumber(user_number))){ 
+                return false;
+            }
+            current_number += user_number;
+        }
+        else if(command == "-"){
+            Number user_number;
+            if(!(ReadNumber(user_number))){
+                return false;
+            }
+            current_number -= user_number;
+        }
+        else if(command == "*"){
+            Number user_number;
+            if(!(ReadNumber(user_number))){
+                return false;
+            }
+            current_number *= user_number;
+        }
+        else if(command == "/"){
+            Number user_number;
+            if(!(ReadNumber(user_number))){
+                return false;
+            }
+            if(user_number == 0){
+                current_number = numeric_limits<Number>::infinity();
+                continue;
+                cout<<"inf"<<endl;
+            }
+            else{
+                current_number /= user_number;
+            } 
+        }
+        else if(command == ":"){
+            Number user_number;
+            if(!(ReadNumber(user_number))){       
+            return false;
+            }
+            current_number = user_number;
+        }
+        else if(command == "c"){
+            current_number = 0;
+        }
+        else if(command == "**"){
+            Number user_number;
+            if(!(ReadNumber(user_number))){
+                return false;
+            }
+            current_number = pow(current_number, user_number);
+        }
+        else if(command == "="){
+            cout<<current_number<<endl;
+        }
+        else if(command == "q"){
+            break;
+        }
+        else{
+            cerr<<"Error: Unknown token "<<command<<endl;
+            return false;
+        }
+    }
+    return true;
+}
