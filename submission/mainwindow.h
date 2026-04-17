e009633c83ae824b7e6f467f99af5d5ac6ab8afe
Slava <viavhaslaukandratsenka@gmail.com>
2026-04-17 23:38:22 +0300

    Update mainwindow.h

--- submission/mainwindow.h
+++ submission/mainwindow.h
@@ -1,80 +1,65 @@
 #pragma once
-#include "calculator.h"
-#include <QMainWindow>
 
+#include "controller.h"
+#include "enums.h"
+
+#include <QMainWindow>
+#include <QComboBox>
+#include <QPushButton>
+#include <QLabel>
+#include <functional>
+#include <string>
+#include <optional>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
-
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
-enum class Operation {
-    NO_OPERATION,   // ожидание ввода операции
-    MULTIPLICATION, // умножение
-    DIVISION,       // деление
-    SUBTRACTION,    // вычитание
-    ADDITION,       // сложение
-    POWER,          // степень
-};
 
-class MainWindow : public QMainWindow {
+class MainWindow : public QMainWindow
+{
     Q_OBJECT
-
-
+    
 public:
-    MainWindow(QWidget* parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
+    
+    
+    void SetInputText(const std::string& text);
+    void SetErrorText(const std::string& text);
+    void SetFormulaText(const std::string& text);
+    void SetMemText(const std::string& text);
+    void SetExtraKey(const std::optional<std::string>& key);
+    
+    
+    void SetDigitKeyCallback(std::function<void(int key)> cb);
+    void SetProcessOperationKeyCallback(std::function<void(Operation key)> cb);
+    void SetProcessControlKeyCallback(std::function<void(ControlKey key)> cb);
+    void SetControllerCallback(std::function<void(ControllerType controller)> cb);
+    
 private slots:
-
-    void numbers_clicked(); // общий обработ для всех цифр
-
-    void on_btn_point_clicked();
-
-    void on_btn_clear_last_number_clicked();
-
-    void on_btn_plus_minus_clicked();
-
-    void on_btn_clear_clicked();
-
-    void on_btn_pow_clicked();
-
-    void on_btn_divide_clicked();
-
-    void on_btn_multiply_clicked();
-
-    void on_btn_minus_clicked();
-
-    void on_btn_plus_clicked();
-
-    void on_btn_equals_clicked();
-
-    void on_btn_mStore_clicked();
-
-    void on_btn_mRecall_clicked();
-
-    void on_btn_mClear_clicked();
-
+   
+    void onDigitClicked(int digit);
+    void onPointClicked();
+    void onOperationClicked(Operation op);
+    void onEqualsClicked();
+    void onClearClicked();
+    void onClearLastNumberClicked();
+    void onPlusMinusClicked();
+    void onMemoryStoreClicked();
+    void onMemoryRecallClicked();
+    void onMemoryClearClicked();
+    void onControllerTypeChanged(int index);
+    
 private:
-    Ui::MainWindow* ui;
-
-    Calculator calculator_;
-    Number active_number_ = 0.0;           // храним текущее активное число в калькуляторе
-    Operation current_operation_ = Operation::NO_OPERATION;  // никаких действий не выполняется
-    void SetText(const QString& text);   // сохранит результат в input_number_ и поместит его в l_result.
-    void AddText(const QString& suffix);// допишет к input_number_ нужный текст и вызовет SetText
-    void SetOperation(Operation op);   // метод вызывается при нажатии всех кнопок операций (+, -, /, *, ^)
-    void PerformOperation(double second_num);  // метод отвечает за выполнение действий (+, -, /, *, ^)
-    QString input_number_;           // храним число как строку, которое вводит пользователь с помощью кнопок
-    QString first_number_str_;      // храним строковую копию первого числа для l_formula
-    QString second_number_str_;    // храним строковую копию второго числа для l_formula
-    QString NormalizeNumber(const QString &text); // метод нармализует числа
-    QString RemoveTrailingZeroes(const QString &text);  // метод удаляет все ненужные 0 из строки и делает числа корректными
-    double first_number_ = 0.0;// числовое знач. первого числа (расчеты)
-    double memory = 0.0;      // храним числа в памяти
-    bool del = true;         // флаг блокировки удаления последних цифр после нажатия кнопки (=)
-    bool in_memory = false; //  устанавливаем изначально флаг о том, что число не в памяти   
-    bool new_input = true; // флаг ввода нового числа                                         
-    bool isTypingNumber_ = false; // флаг который указывает начал ли user ввод чисел          
+    Ui::MainWindow *ui;
+    
+    QComboBox *cmb_controller;
+    QPushButton *tb_extra;
+    
+    std::function<void(int key)> digitCallback;
+    std::function<void(Operation key)> operationCallback;
+    std::function<void(ControlKey key)> controlCallback;
+    std::function<void(ControllerType type)> controllerCallback;
+    
+    void* controller = nullptr;
 };
