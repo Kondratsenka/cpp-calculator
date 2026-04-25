17fa211fb9e6dc77db0d3d6cafbfc9a94827147d
Slava <viavhaslaukandratsenka@gmail.com>
2026-04-25 23:56:05 +0300

    Update mainwindow.h

--- submission/mainwindow.h
+++ submission/mainwindow.h
@@ -1,8 +1,6 @@
 #pragma once
 
-#include "controller.h"
 #include "enums.h"
-
 #include <QMainWindow>
 #include <QComboBox>
 #include <QPushButton>
@@ -18,26 +16,24 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-    
+
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-    
-    
+
+
     void SetInputText(const std::string& text);
     void SetErrorText(const std::string& text);
     void SetFormulaText(const std::string& text);
     void SetMemText(const std::string& text);
     void SetExtraKey(const std::optional<std::string>& key);
-    
-    
     void SetDigitKeyCallback(std::function<void(int key)> cb);
     void SetProcessOperationKeyCallback(std::function<void(Operation key)> cb);
     void SetProcessControlKeyCallback(std::function<void(ControlKey key)> cb);
     void SetControllerCallback(std::function<void(ControllerType controller)> cb);
-    
+
 private slots:
-   
+
     void onDigitClicked(int digit);
     void onPointClicked();
     void onOperationClicked(Operation op);
@@ -49,17 +45,16 @@ private slots:
     void onMemoryRecallClicked();
     void onMemoryClearClicked();
     void onControllerTypeChanged(int index);
-    
+
 private:
     Ui::MainWindow *ui;
-    
+
     QComboBox *cmb_controller;
     QPushButton *tb_extra;
-    
+
     std::function<void(int key)> digitCallback;
     std::function<void(Operation key)> operationCallback;
     std::function<void(ControlKey key)> controlCallback;
     std::function<void(ControllerType type)> controllerCallback;
-    
-    void* controller = nullptr;
+
 };
